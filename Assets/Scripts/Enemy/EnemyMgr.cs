53f924c12c836b3593ec0a60878148bfb31971eb
LUCIAN <jzhao0538@gmail.com>
2023-03-01 00:55:38 +0800

    InitCommit

---
+++ Assets/Scripts/Enemy/EnemyMgr.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Enemy
+{
+    struct Enemy
+    {
+        private float blood;
+        private float attack;
+    }
+    public class EnemyMgr : MonoBehaviour
+    {
+        
+    }
+}
